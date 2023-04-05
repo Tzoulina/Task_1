37ef65eab009cc37c452a88915d5d9fbafa2a60f
Tzoulina <105767955+Tzoulina@users.noreply.github.com>
2023-04-05 19:29:10 +0800

    Add files via upload
    Task 1: Understanding Props
    1. Create a new ReactJS component called Greeting that takes a name prop and displays a 
    personalized greeting message.
    2. In the App component, render the Greeting component with different name props, such 
    as "John", "Mary", "Alex", and "Sarah".
    3. Observe how the Greeting component updates the message based on the name prop 
    passed to it

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="John" />
+      <Greeting name="Mary" />
+      <Greeting name="Alex" />
+      <Greeting name="Sarah" />
+    </div>
+  );
+}
+
+export default App;
